ad3bc0856f37e28400d67537b76f4d6f2b7905b7
tgross03 <tom.gross@udo.edu>
2025-04-28 19:43:53 +0200

    Initial commit

---
+++ src/main/java/dev/edgetom/questFlow/QuestFlow.java
@@ -0,0 +1,17 @@
+package dev.edgetom.questFlow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuestFlow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
